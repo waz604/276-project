0d5e50be2cff229351ab1b3109e5e1d8dd658589
jimmyh <hoang.jimmy8@gmail.com>
2026-03-09 08:43:40 -0700

    init springboot project

---
+++ studbuds/src/main/java/com/cmpt276/studbuds/StudbudsApplication.java
@@ -0,0 +1,13 @@
+package com.cmpt276.studbuds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudbudsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudbudsApplication.class, args);
+	}
+
+}
---
+++ studbuds/src/test/java/com/cmpt276/studbuds/StudbudsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmpt276.studbuds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudbudsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
